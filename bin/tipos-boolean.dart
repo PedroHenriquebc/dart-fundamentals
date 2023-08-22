58ce705ce9c1f3e50f8a427aa8bfe9c70ab857e3
Pedro Henrique <pedrohbc96@gmail.com>
2023-08-22 19:48:04 -0300

    feat: adiciona tipos de dados booleanos

---
+++ bin/tipos-boolean.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  var varTrue = true;
+  bool varFalse = false; //o valor padrao de um booleano é falso, nao necessitando a atribuição para este valor
+
+  print(varTrue);
+  print(varFalse);
+  print(!varTrue);
+  print(!varFalse);
+}
