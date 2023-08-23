900f0588493f11222f396a6c9f99a1703179e139
Pedro Henrique <pedrohbc96@gmail.com>
2023-08-22 21:44:57 -0300

    feat: adiciona tipos de dados constantes

---
+++ bin/tipos-constantes.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  const String variavel = "ABC";
+  const int variavel1 = 100;
+  const bool variavel2 = true;
+
+  variavel = "CDE"; // erro pois um valor constante não pode ser alterado
+}
